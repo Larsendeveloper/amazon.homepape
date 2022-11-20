ed4720703b79ade7e0719dbcf67ecacece3ea786
Larsendeveloper <larsendacosta111@gmail.com>
2022-11-20 08:33:40 -0800

    homepage

---
+++ home.js
@@ -0,0 +1,35 @@
+// porducts image slider
+const productContainers = [...document.querySelectorAll('.product-container')];
+const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
+const preBtn = [...document.querySelectorAll('.pre-btn')];
+
+productContainers.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+
+    nxtBtn[i].addEventListener('click', () => {
+        item.scrollLeft += containerWidth;
+    })
+
+    preBtn[i].addEventListener('click', () => {
+        item.scrollLeft -= containerWidth;
+    })
+})
+
+// porducts image slider with details
+const productContainers2 = [...document.querySelectorAll('.product-container2')];
+const nxtBtn2 = [...document.querySelectorAll('.nxt-btn2')];
+const preBtn2 = [...document.querySelectorAll('.pre-btn2')];
+
+productContainers2.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+
+    nxtBtn2[i].addEventListener('click', () => {
+        item.scrollLeft += containerWidth;
+    })
+
+    preBtn2[i].addEventListener('click', () => {
+        item.scrollLeft -= containerWidth;
+    })
+})
