606f424018e0e9b2561e9f428cd35c6c61b6dce1
Larsendeveloper <larsendacosta111@gmail.com>
2022-12-07 23:29:25 -0800

    weather

--- home.js
+++ home.js
@@ -55,3 +55,37 @@ const changemode = () => {
 
  mybody.classList.toggle('myDark')
 }
+
+
+// weather info
+
+let x = document.getElementById('out');
+let y = document.getElementById('Weather');
+
+function geolocation(){
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(showPosition)
+    }else{
+        x.innerText = "Geo Not Supported"
+    }
+}
+
+function showPosition(data){
+    console.log(data);
+    let lat = data.coords.latitude;
+    let long = data.coords.longitude;
+    let out = `Latitude is ${lat} & longitude is ${long}`
+    
+    const url = `https://api.openweathermap.org/data/2.5/forecast/daily?lat=${lat}&lon=${long}&mode=json&units=metric&cnt=5&appid=fbf712a5a83d7305c3cda4ca8fe7ef29`
+    ///api calling
+    fetch(url,{method: 'GET'})
+    //return promise
+    .then((res) => res.json())
+    //return data
+    .then((data) => {
+        console.log(data)
+        let city = data.city.name
+        let temp = data.list[0].temp.day
+        y.innerText = `Your city is ${city} and temp is ${temp}°C`
+    })
+}
